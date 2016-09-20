a56b9ba6c01669b38c8f8a98fc38490daf1dc994
ffs <ffs.email>
2016-09-20 17:55:18 +0800

    [shell] update

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ do
 		then
 
 		i=$[$i+1]
-		
+		i=20
 		echo $i
 		tar cpf - "${line}" | tar xpf - -C ./
         if [ "$i" = "20" ];
@@ -20,7 +20,7 @@ do
 			git add -A
 			git commit  -a -m "[DTV-UPDAE] "$line" "
 			git push origin master
-			i=0
+			i=20
 		fi
 	fi
 done
